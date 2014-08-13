53d8a02db3ddddfc5da24f0f98f2cf17306456e7
bfzhao <bfzhao.h@gmail.com>
2014-08-13 10:22:38 +0800

    添加工作分支dev

--- algorithm/sort/SwapSort.cc
+++ algorithm/sort/SwapSort.cc
@@ -94,9 +94,33 @@ void SimpleSelect(T* inPtr, int seqLen)
 
 /*******************************************************************************
  * QuickSort:
- *  
+ * --主要思想：
+ *          快速排序是C.R.A.Hoare于1962年提出的一种划分交换排序。它采用了一种分治的策略，
+ *          通常称其为分治法(Divide-and-ConquerMethod)。
+ * --分治：
+ *          在R[low..high]中任选一个记录作为基准(Pivot)，以此基准将当前无序区划分为左、
+ *          右两个较小的子区间R[low..pivotpos-1)和R[pivotpos+1..high]，并使左边子
+ *          区间中所有记录的关键字均小于等于基准记录(不妨记为pivot)的关键字pivot.key，
+ *          右边的子区间中所有记录的关键字均大于等于pivot.key，而基准记录pivot则位于正
+ *          确的位置(pivotpos)上，它无须参加后续的排序。
+ *          注意：
+ *  　           划分的关键是要求出基准记录所在的位置pivotpos。划分的结果可以简单地表示为
+ *          (注意pivot=R[pivotpos])：
+ *          R[low..pivotpos-1].keys≤R[pivotpos].key≤R[pivotpos+1..high].keys
+ *          其中low≤pivotpos≤high。
+ * --求解：
+ *          通过递归调用快速排序对左、右子区间R[low..pivotpos-1]和R[pivotpos+1..high]
+ *          快速排序。
+ * --组合：
+ *          因为当"求解"步骤中的两个递归调用结束时，其左、右两个子区间已有序。对快速排序而言，
+ *          "组合"步骤无须做什么，可看作是空操作。
  ******************************************************************************/
-
+template <typename T>
+int Partion(T* inPtr, int lowIdx, int highIdx)
+{
+    int tmpIdx = lowIdx;
+    
+}
 
 int main(int argc, char const *argv[])
 {
@@ -104,15 +128,15 @@ int main(int argc, char const *argv[])
     int seq[seqLen] = {0};
     int seqb[seqLen] = {0};
     RandSeqGenerator(seq,seqLen,0,seqLen);
-    memcpy(seqb,seq,sizeof(int)*seqLen);
+    // memcpy(seqb,seq,sizeof(int)*seqLen);
     copy(seq, seq + seqLen, ostream_iterator<int>(cout,", ")); cout<<endl;
-    BubbleSortWithFlag2(seq,seqLen);
-    cout<<swapCounter<<endl;
-    assert(IsUpOrdered(seq,seqLen));
-    swapCounter = 0;
-    BubbleSort(seqb,seqLen);
-    cout<<swapCounter<<endl;
-    assert(IsUpOrdered(seqb,seqLen));
+    // BubbleSortWithFlag2(seq,seqLen);
+    // cout<<swapCounter<<endl;
+    // assert(IsUpOrdered(seq,seqLen));
+    // swapCounter = 0;
+    // BubbleSort(seqb,seqLen);
+    // cout<<swapCounter<<endl;
+    // assert(IsUpOrdered(seqb,seqLen));
     // copy(seq, seq + seqLen, ostream_iterator<int>(cout,", ")); cout<<endl;
     cout<<"\nMain Finished"<<endl;
     return 0;
